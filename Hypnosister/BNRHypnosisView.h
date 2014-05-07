23333e77cb81762036710a7eaf7d5e215ea0964c
Raymon Atienza <raymon.atienza@gmail.com>
2014-05-07 01:38:17 -0400

    started hypnosister up to drawing concentric circles

---
+++ Hypnosister/BNRHypnosisView.h
@@ -0,0 +1,13 @@
+//
+//  BNRHypnosisView.h
+//  Hypnosister
+//
+//  Created by Raymon Atienza on 2014-05-07.
+//  Copyright (c) 2014 Raymon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRHypnosisView : UIView
+
+@end
